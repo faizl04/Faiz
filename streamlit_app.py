86b7dd733ca44429d0b7ce7266858ffcfbf8f44f
faizl04 <mfl.lukmanulhakim@gmail.com>
2025-05-22 10:59:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 paisss")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "mari ngoding bersama pais"
 )
